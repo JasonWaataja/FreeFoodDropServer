f44d4ceefea9654bd43e5f54ebb3fad1b2151f36
Jason Waataja <jasonswaataja@gmail.com>
2016-10-18 18:18:42 -0700

    READ THIS, changed to BSD style
    Changed a bunch of stuff so that it follows the FreeBSD coding style.
    This includes moving things around, declaring things static, organizing
    things alphabetically such as the includes, formatting comments,
    changing indentation, etc.
    
    If you want to use the same formatting in vim, use these settings in
    file called ~/.vim/after/ftplugin/c.vim:
    
    setlocal cindent
    setlocal cinoptions=>4^4(4+4
    
    set formatoptions=cqt
    set shiftwidth=4
    set smarttab
    set textwidth=79

--- src/main.c
+++ src/main.c
@@ -29,18 +29,18 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <err.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <string.h>
+
+#include <err.h>
 #include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /* The file descriptor for the incoming socket.  Being something other than -1
  * means that it hasn't been set yet and doesn't need to be closed.  */
@@ -50,158 +50,160 @@ int sockfd = -1;
 int should_continue = 1;
 
 /* If this gives an error, used -std=gnu99 */
-#define SERVER_PORT "8110"
+#define 	SERVER_PORT "8110"
 
 /* The amount of connections to keep in the backlog.  */
-#define BACKLOG 20
+#define 	BACKLOG 20
 
-/* Set up the networking.  Creates the port and sets sockfd to the value.
- * Exits the program if there's an error.  */
-void
-init_networking ();
+static void	close_socket();
+static void	handle_connection(int socket);
+static void	init_networking();
+static void	main_loop();
+static void	print_address(FILE *stream, struct sockaddr *sockaddr);
+static void	signal_handler(int signum);
+
+/* 
+ * Set up the networking.  Creates the port and sets sockfd to the value.
+ * Exits the program if there's an error.
+ */
+static void
+init_networking()
+{
+		struct addrinfo *host_addr, hints;
+		int status, sockfd;
+
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_family = AF_UNSPEC;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_flags = AI_PASSIVE;
+
+		status = getaddrinfo(NULL, SERVER_PORT, &hints, &host_addr);
+
+		if (status == -1)
+			err(1, "Failed to create address");
+
+		sockfd = socket(host_addr->ai_family, host_addr->ai_socktype,
+			host_addr->ai_protocol);
+
+		if (sockfd == -1)
+			err(1, "Failed to create socket");
+
+		status = bind(sockfd, host_addr->ai_addr, host_addr->ai_addrlen);
+
+		if (status == -1)
+			err(1, "Failed to bind port %s", SERVER_PORT);
+
+		freeaddrinfo(host_addr);
+
+		signal(SIGINT, signal_handler);
+		signal(SIGTERM, signal_handler);
+
+		listen(sockfd, BACKLOG);
+}
+
+/*
+ * This was mainly intended to handle termination signals so that it can exit
+ * the infinite loop gracefully.
+ */
+static void
+signal_handler(int signum)
+{
+
+		close_socket();
+		/* Do a bunch of stuff to terminate the worker threads here.  */
+
+		/* Set the signal back to its original value and use that.  */
+		signal (signum, SIG_DFL);
+		raise (signum);
+}
 
 /* Start accepting connections.  Exists the program if there's an error.  */
-void
-main_loop ();
+static void
+main_loop()
+{
+		struct sockaddr_storage their_sock;
+		socklen_t their_socklen;
+		int newfd;
+
+		/* Loop forever until should_continue is set to false.  */
+		while (should_continue) {
+			printf("Accepting connections.\n");
+
+			/* Accept a connection.  */
+			newfd = accept (sockfd, (struct sockaddr *)&their_sock,
+				&their_socklen);
+
+			if (newfd == -1)
+				warn ("Failed to accept connection");
+
+			printf("Got a connection from ");
+			print_address(stdout, (struct sockaddr *)&their_sock);
+			printf("\n");
+
+			handle_connection(newfd);
+		}
+
+		close_socket();
+}
 
 /* Closes the socket, supposed to be on exit.  */
-void
-close_socket ();
+static void
+close_socket()
+{
 
-/* Prints the address (inet or inet6) to the given stream.  I decided to it this
+		if (sockfd > -1)
+			close (sockfd);
+
+		sockfd = -1;
+}
+
+/*
+ * Prints the address (inet or inet6) to the given stream.  I decided to it this
  * way because returning a char array would mean that it would need to be freed
- * with free () which is annoying and ties it to malloc () (as opposed to c++'s
- * delete.  */
-void
-print_address (FILE *stream, struct sockaddr *sockaddr);
-
-void
-handle_connection (int socket);
-
-void
-signal_handler (int signum);
-
-void
-init_networking ()
+ * with free() which is annoying and ties it to malloc () (as opposed to c++'s
+ * delete.
+ */
+static void
+print_address(FILE *stream, struct sockaddr *sockaddr)
 {
-  struct addrinfo *host_addr = NULL;
 
-  struct addrinfo hints;
-  memset (&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
+		if (sockaddr->sa_family == AF_INET)
+		{
+			struct sockaddr_in *as_inet = (struct sockaddr_in *)sockaddr;
+			char ip[INET_ADDRSTRLEN];
 
-  int status;
-  status = getaddrinfo (NULL, SERVER_PORT, &hints, &host_addr);
+			inet_ntop (AF_INET, &(as_inet->sin_addr), ip, INET_ADDRSTRLEN);
 
-  if (status == -1)
-	err (EXIT_FAILURE, "Failed to create address");
+			fprintf (stream, "%s", ip);
+		}
+		else if (sockaddr->sa_family == AF_INET6)
+		{
+			struct sockaddr_in6 *as_inet6 = (struct sockaddr_in6 *)sockaddr;
+			char ip[INET6_ADDRSTRLEN];
 
-  sockfd = socket (host_addr->ai_family,
-				   host_addr->ai_socktype,
-				   host_addr->ai_protocol);
+			inet_ntop(AF_INET6, &(as_inet6->sin6_addr), ip, INET6_ADDRSTRLEN);
 
-  if (sockfd == -1)
-	err (EXIT_FAILURE, "Failed to create socket");
-
-  status = bind (sockfd, host_addr->ai_addr, host_addr->ai_addrlen);
-
-  if (status == -1)
-	err (EXIT_FAILURE, "Failed to bind port %s", SERVER_PORT);
-
-  freeaddrinfo (host_addr);
-
-  signal (SIGINT, signal_handler);
-  signal (SIGTERM, signal_handler);
-
-  listen (sockfd, BACKLOG);
+			fprintf(stream, "%s", ip);
+		}
 }
 
-void
-signal_handler (int signum)
+/*
+ * Handle the conection already accepted with socket. This function is
+ * responsible for closing the socket.
+ */
+static void
+handle_connection(int socket)
 {
-  close_socket ();
-  /* Do a bunch of stuff to terminate the worker threads here.  */
-
-  /* Set the signal back to its original value and use that.  */
-  signal (signum, SIG_DFL);
-  raise (signum);
+		/* TODO: Handle the connection here. */
+		close(socket);
 }
 
-void
-main_loop ()
-{
-  /* Loop forever until should_continue is set to false.  */
-  while (should_continue)
-	{
-	  /* The file descriptor for the new connection.  */
-	  int newfd;
-
-	  struct sockaddr_storage their_sock;
-	  socklen_t their_socklen;
-
-          printf ("Accepting connections.\n");
-
-	  /* Accept a connection.  */
-	  newfd = accept (sockfd,
-					  (struct sockaddr *) &their_sock,
-					  &their_socklen);
-
-	  if (newfd == -1)
-		warn ("Failed to accept connection");
-
-	  printf ("Got a connection from ");
-	  print_address (stdout, (struct sockaddr *) &their_sock);
-	  printf ("\n");
-
-	  handle_connection (newfd);
-	}
-
-  close_socket ();
-}
-
-void
-close_socket ()
-{
-  if (sockfd > -1)
-	close (sockfd);
-
-  sockfd = -1;
-}
-
-void
-print_address (FILE *stream, struct sockaddr *sockaddr)
-{
-  if (sockaddr->sa_family == AF_INET)
-	{
-	  struct sockaddr_in *as_inet = (struct sockaddr_in *) sockaddr;
-	  char ip[INET_ADDRSTRLEN];
-
-	  inet_ntop (AF_INET, &(as_inet->sin_addr), ip, INET_ADDRSTRLEN);
-
-	  fprintf (stream, "%s", ip);
-	}
-  else if (sockaddr->sa_family == AF_INET6)
-	{
-	  struct sockaddr_in6 *as_inet6 = (struct sockaddr_in6 *) sockaddr;
-	  char ip[INET6_ADDRSTRLEN];
-
-	  inet_ntop (AF_INET6, &(as_inet6->sin6_addr), ip, INET6_ADDRSTRLEN);
-
-	  fprintf (stream, "%s", ip);
-	}
-}
-
-void
-handle_connection (int socket)
-{
-  /* Handle connection here.  Do some threading stuff.  */
-  close (socket);
-}
+/*
+ * TODO: Put the program description here.
+ */
 int
-main (int argc, char *argv[])
+main(int argc, char *argv[])
 {
-  init_networking ();
-  main_loop ();
+		init_networking ();
+		main_loop ();
 }
